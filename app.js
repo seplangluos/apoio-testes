afd3156a36595de39f429b1c5d614c2d2c3b38ba
seplangluos <seplan.gluos@valadares.mg.gov.br>
2026-07-21 09:29:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -55,7 +55,8 @@ const USER_EMAIL_MAPPING = {
   "Rita": "rita@hotmail.com",
   "Mara": "mara@hotmail.com",
   "Tati": "tati@hotmail.com",
-  "Gabriela": "gabriela@hotmail.com",
+  "Gabriella": "gabriela@hotmail.com",
+  "Fabiano": "fabiano@hotmail.com",
   "Admin": "seplan.gluos@valadares.mg.gov.br"
 };
 
@@ -71,7 +72,7 @@ function emailToUsername(email) {
 
 // Dados da aplicação
 const GLUOS_DATA = {
-  usuarios: ["Eduardo", "Wendel", "Júlia", "Tati", "Sônia", "Rita", "Mara", "Gabriela", "Admin"],
+  usuarios: ["Eduardo", "Wendel", "Júlia", "Tati", "Sônia", "Rita", "Mara", "Gabriella", "Fabiano", "Admin"],
   assuntos: [
     {id: 1, texto: "Separar e Preparar os Processos Agendados no Dia"},
     {id: 2, texto: "Inserção de Avisos de Vistoria na E&L"},
